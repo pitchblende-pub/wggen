c63c12abc7c7e5c7d843c74b3cafd6b59a701772
Tanaka Masaki <pitchblende@kogarashi.net>
2022-07-17 00:38:43 +0900

    i[date

--- wggen.sh
+++ wggen.sh
@@ -4,10 +4,8 @@ Peers=10 # 設定するクライアントの数。1〜9999の範囲。
 ServerConfigFile=wg0.conf # /etc/wireguardに置くファイルの名前
 ServerPort=51820 # Wireguardが使用する実ポート
 Endpoint=example.ddns.jp:51820 # 外部から見た場合のサーバーアドレスとポート番号
-
-# 次の0行は、他のマシンをサーバーにする場合は必ず手動で設定
-#EthernetInterface=eth0 # サーバーから外部にアクセスするための実インターフェイス
-#DNS=192.168.1.1 # サーバーが参照するネームサーバー
+EthernetInterface=eth0 # サーバーから外部にアクセスするための実インターフェイス
+DNS=192.168.1.1 # サーバーが参照するネームサーバー
 
 #トンネルとして使う仮想インターフェイスのアドレス($iはクライアント番号で置き換えられる。)
 ServerWgAddress='10.0.100.1/16, fdfd:abcd:1::a000/96'
@@ -18,7 +16,7 @@ ServerAllowedIPs='10.0.$((i/100)).$((i%100))/32, fdfd:abcd:1::$i/128'
 ClientAllowedIPs='10.0.0.0/16, fdfd:abcd:1::0/96, 192.168.1.0/24' # LAN内向けアクセスのみをトンネルさせる場合（192.168.1.0/24は使用環境のネットワークアドレスに合わせること）
 #ClientAllowedIPs='0.0.0.0/0,::0' # 全アクセスをトンネルさせる場合
 
-GenPSK=true #事前共有鍵を生成するか否か
+UsePSK=true #事前共有鍵を使用するか否か
 OutputDir='output' # 設定ファイルの出力先。絶対パスまたは本スクリプトからの相対パス。
 
 cd `dirname $0` || exit 1
@@ -27,14 +25,6 @@ if [ -z $EthernetInterface ]; then
 	EthernetInterface=$(for d in `find /sys/devices -name net | grep -m1 -v virtual`; do ls $d/; done)
 	if [ -z $EthernetInterface ]; then
 		echo "EthernetInterfaceを指定してください。"
-		exit 1
-	fi
-fi
-
-if [ -z $DNS ]; then
-	DNS=$(resolvectl dns $EthernetInterface |sed -ne 's/.\+:[\t ]\+\([0-9a-f.:]\+\)[\t ]*$/\1/p')
-	if [ -z $DNS ]; then
-		echo "DNSを指定してください。"
 		exit 1
 	fi
 fi
@@ -52,16 +42,14 @@ for i in $(seq  $Peers) ; do
 	base=$(printf %04d $i)
 	if [ ! -f ${OutputDir}/keys/$base.txt ]; then
 		pubkey=$(wg genkey |tee ${OutputDir}/keys/$base.txt|wg pubkey)
-		if "${GenPSK}"; then
-			psk=$(wg genpsk)
-			echo ${pubkey}$'\n'${psk}>> ${OutputDir}/keys/$base.txt || exit 1
-		fi
+		echo ${pubkey}$'\n'$(wg genpsk) >> ${OutputDir}/keys/$base.txt||exit 1
         fi
 done
 
 keys=($(cat ${OutputDir}/keys/server.txt))
 ServerPrivatekey=${keys[0]}
 ServerPublickey=${keys[1]}
+PSKkey=${keys[2]}
 
 ### サーバー設定ファイルの[Interface]部分を出力
 # クライアント→LAN内はNATでアクセス、クライアントへは他クライアント含め自由にアクセス可という設定。
@@ -80,14 +68,14 @@ for i in $(seq $Peers) ; do
 	keys=($(cat ${OutputDir}/keys/$base.txt)) || exit 1
 	ClientPrivatekey=${keys[0]}
 	ClientPublickey=${keys[1]}
-	if [ -z ${keys[2]} ]; then
-		psk="#TBDel"
+	if "$UsePSK"; then
+		psk="PresharedKey = $PSKkey"
 	else
-		psk="PresharedKey = ${keys[2]}"
+		psk="#PresharedKey = $PSKkey"
 	fi
 
 	### Peer設定ファイルを出力
-	cat <<-EOF2 |sed -e '/^#TBDel/d' > ${OutputDir}/c${base}.conf || exit 1
+	cat <<-EOF2 > ${OutputDir}/c${base}.conf || exit 1
 	[Interface]
 	PrivateKey = $ClientPrivatekey
 	Address = $(eval echo $ClientWgAddress)
